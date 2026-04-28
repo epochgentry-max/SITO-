4c356188395a99317de155b8ef446bde906219e6
epochgentry-max <epochgentry@gmail.com>
2026-04-28 14:32:31 -0500

    Update input.js

--- input.js
+++ input.js
@@ -134,7 +134,9 @@ if (togglePassword && passwordField) {
         console.log("SITO: Sesión activa para ID:", data.id_interno);
         
         // Llamamos a nuestra alerta personalizada
-        mostrarSitoAlert('Acceso Concedido. Bienvenido, Aliado', + data.reg_username + data.id_interno, '✅');
+        // BLOQUE DE REEMPLAZO
+mostrarSitoAlert('Acceso Concedido. Bienvenido, Aliado', `${data.reg_username} (ID: ${data.id_interno})`, '✅');
+        
         
       } catch (err) {
         mostrarSitoAlert('Fallo crítico de comunicación con el Núcleo SITO.', '☢️');
